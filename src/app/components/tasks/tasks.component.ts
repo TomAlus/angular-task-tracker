86ade41ff62946004751862e7d9da47239cb86e8
Tomasz Klekner <tomasz.klekner@gmail.com>
2022-07-10 20:23:32 +0200

    Add task service and use it

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
 
 import { Task } from 'src/app/Task';
 import { TASKS } from '../../mock-tasks';
@@ -9,9 +10,11 @@ import { TASKS } from '../../mock-tasks';
   styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() {}
+  constructor(private taskService: TaskService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
 }
