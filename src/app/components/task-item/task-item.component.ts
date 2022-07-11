cd6fbc8e9040a56b027fc13c8ea97c6a4db62429
Tomasz Klekner <tklekner@objectivity.co.uk>
2022-07-11 11:10:12 +0200

    Add toggle reminder on task

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -11,6 +11,7 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 export class TaskItemComponent implements OnInit {
   @Input('task') task!: Task;
   @Output() onDeleteTask = new EventEmitter<Task>();
+  @Output() onToggleReminder = new EventEmitter<Task>();
 
   faTimes = faTimes;
 
@@ -19,6 +20,10 @@ export class TaskItemComponent implements OnInit {
   ngOnInit(): void {}
 
   onDelete(task: Task) {
-    this.onDeleteTask.emit(task)
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
   }
 }
